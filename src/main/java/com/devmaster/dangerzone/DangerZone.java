da9575b8c95ec7ad7000f24047d5186464d44c1a
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-11-18 20:46:19 -0500

    Got worspace set up
    welcome to 1.15.2

---
+++ src/main/java/com/devmaster/dangerzone/DangerZone.java
@@ -0,0 +1,66 @@
+package com.devmaster.dangerzone;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import static com.devmaster.dangerzone.DangerZone.MODID;
+
+@Mod(value = MODID)
+public class DangerZone {
+    public static final String MODID = "dangerzone";
+
+        public static boolean debug;
+
+        public static ItemGroup DEFAULT_GROUP;
+        public static ItemGroup EGG_ORE_GROUP;
+
+        //public static DangerZoneConfig config;
+
+        public DangerZone() {
+
+            debug = true;
+
+            setupConfig();
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+            FMLJavaModLoadingContext.get().getModEventBus().addListener(this::finishLoading);
+            MinecraftForge.EVENT_BUS.register(this);
+
+            //DEFAULT_GROUP = new ItemGroupDangerZone.DangerZoneGroup();
+           // EGG_ORE_GROUP = new ItemGroupDangerZone.EggOreGroup();
+        }
+
+        private void setup(final FMLCommonSetupEvent event) {
+          //  CalendarUtils.dayCheck();
+          //  WorldGenOres.setupOreGen();
+        }
+
+        private void clientSetup(final FMLClientSetupEvent event) {
+          //  DangerZoneBlocks.setRenderLayers();
+           // DangerZoneEntities.registerEntityRenders();
+          // DangerZoneContainerTypes.registerScreens();
+        }
+
+        private void finishLoading(final FMLLoadCompleteEvent event) {
+
+        }
+
+        private void setupConfig() {
+            ForgeConfigSpec.Builder configBuilder = new ForgeConfigSpec.Builder();
+
+          //  config = new DangerZoneConfig(configBuilder);
+
+            ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, configBuilder.build(), MODID + ".toml");
+        }
+
+    }
+
+//}
