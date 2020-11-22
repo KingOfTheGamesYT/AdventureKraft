46107b7436bfe03acbab123bde20313936565e2c
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-11-21 23:40:38 -0500

    Added Petrified cod, mule, pufferfish, salmon, silverfish, skeleton horse, trader llama, trpoical fish, vex, wandering trader, zombie horse, villager zombie, to the config
    you can disable them

--- src/main/java/com/devmaster/dangerzone/init/DangerZoneConfig.java
+++ src/main/java/com/devmaster/dangerzone/init/DangerZoneConfig.java
@@ -20,8 +20,10 @@ public class DangerZoneConfig {
 
 	public BooleanValue batOre;
 	public BooleanValue blazeOre;
+	public BooleanValue catOre;
 	public BooleanValue cavespiderOre;
 	public BooleanValue chickenOre;
+	public BooleanValue codOre;
 	public BooleanValue cowOre;
 	public BooleanValue creeperOre;
 	public BooleanValue dolphinOre;
@@ -30,6 +32,7 @@ public class DangerZoneConfig {
 	public BooleanValue elderGuardianOre;
 	public BooleanValue enderDragonOre;
 	public BooleanValue endermanOre;
+	public BooleanValue endermiteOre;
 	public BooleanValue evokerOre;
 	public BooleanValue guardianOre;
 	public BooleanValue horseOre;
@@ -38,31 +41,40 @@ public class DangerZoneConfig {
 	public BooleanValue llamaOre;
 	public BooleanValue magmacubeOre;
 	public BooleanValue mooshroomOre;
+	public BooleanValue muleOre;
 	public BooleanValue ocelotOre;
 	public BooleanValue pandabearOre;
 	public BooleanValue parrotOre;
 	public BooleanValue phantomOre;
 	public BooleanValue pigOre;
 	public BooleanValue polarbearOre;
+	public BooleanValue pufferfishOre;
 	public BooleanValue rabbitOre;
+	public BooleanValue salmonOre;
 	public BooleanValue sheepOre;
+	public BooleanValue silverfishOre;
 	public BooleanValue skeletonOre;
+	public BooleanValue skeletonHorseOre;
 	public BooleanValue slimeOre;
 	public BooleanValue snowGolemOre;
 	public BooleanValue spiderOre;
 	public BooleanValue squidOre;
 	public BooleanValue strayOre;
 	public BooleanValue turtleOre;
+	public BooleanValue traderLlamaOre;
+	public BooleanValue tropicalFishOre;
+	public BooleanValue vexOre;
 	public BooleanValue villagerOre;
 	public BooleanValue vindicatorOre;
+	public BooleanValue wanderingTraderOre;
 	public BooleanValue witchOre;
 	public BooleanValue witherOre;
 	public BooleanValue witherSkeletonOre;
 	public BooleanValue wolfOre;
 	public BooleanValue zombieOre;
+	public BooleanValue zombieHorseOre;
 	public BooleanValue zombiePigmanOre;
-	public BooleanValue catOre;
-	public BooleanValue endermiteOre;
+	public BooleanValue zombieVillagerOre;
 	public BooleanValue foxOre;
 	public BooleanValue ghastOre;
 	public BooleanValue pillagerOre;
@@ -93,55 +105,128 @@ public class DangerZoneConfig {
 		builder.push("egg_ore_world_gen");
 
 		batOre = addOreConfig(builder, "Bat");
+
 		blazeOre = addOreConfig(builder, "Blaze");
-		cavespiderOre = addOreConfig(builder, "Cave", "Spider");
-		chickenOre = addOreConfig(builder, "Chicken");
-		cowOre = addOreConfig(builder, "Cow");
-		creeperOre = addOreConfig(builder, "Creeper");
-		dolphinOre = addOreConfig(builder, "Dolphin");
-		donkeyOre = addOreConfig(builder, "Donkey");
-		drownedOre = addOreConfig(builder, "Drowned");
-		elderGuardianOre = addOreConfig(builder, "Elder", "Guardian");
-		enderDragonOre = addOreConfig(builder, "Ender", "Dragon");
-		endermanOre = addOreConfig(builder, "Enderman");
-		evokerOre = addOreConfig(builder, "Evoker");
-		guardianOre = addOreConfig(builder, "Guardian");
-		horseOre = addOreConfig(builder, "Horse");
-		huskOre = addOreConfig(builder, "Husk");
-		ironGolemOre = addOreConfig(builder, "Iron", "Golem");
-		llamaOre = addOreConfig(builder, "Llama");
-		magmacubeOre = addOreConfig(builder, "Magma", "Cube");
-		mooshroomOre = addOreConfig(builder, "Mooshroom");
-		ocelotOre = addOreConfig(builder, "Ocelot");
-		pandabearOre = addOreConfig(builder, "Panda", "Bear");
-		parrotOre = addOreConfig(builder, "Parrot");
-		phantomOre = addOreConfig(builder, "Phantom");
-		pigOre = addOreConfig(builder, "Pig");
-		polarbearOre = addOreConfig(builder, "Polar", "Bear");
-		rabbitOre = addOreConfig(builder, "Rabbit");
-		sheepOre = addOreConfig(builder, "Sheep");
-		skeletonOre = addOreConfig(builder, "Skeleton");
-		slimeOre = addOreConfig(builder, "Slime");
-		snowGolemOre = addOreConfig(builder, "Snow", "Golem");
-		spiderOre = addOreConfig(builder, "Spider");
-		squidOre = addOreConfig(builder, "Squid");
-		strayOre = addOreConfig(builder, "Stray");
-		turtleOre = addOreConfig(builder, "Turtle");
-		villagerOre = addOreConfig(builder, "Villager");
-		vindicatorOre = addOreConfig(builder, "Vindicator");
-		witchOre = addOreConfig(builder, "Witch");
-		witherOre = addOreConfig(builder, "Wither");
-		witherSkeletonOre = addOreConfig(builder, "Wither", "Skeleton");
-		wolfOre = addOreConfig(builder, "Wolf");
-		zombieOre = addOreConfig(builder, "Zombie");
-		zombiePigmanOre = addOreConfig(builder, "Zombie", "Pigman");
+
 		catOre = addOreConfig(builder, "Cat");
+
+		cavespiderOre = addOreConfig(builder, "Cave", "Spider");
+
+		chickenOre = addOreConfig(builder, "Chicken");
+
+		codOre = addOreConfig(builder, "Cod");
+
+		cowOre = addOreConfig(builder, "Cow");
+
+		creeperOre = addOreConfig(builder, "Creeper");
+
+		dolphinOre = addOreConfig(builder, "Dolphin");
+
+		donkeyOre = addOreConfig(builder, "Donkey");
+
+		drownedOre = addOreConfig(builder, "Drowned");
+
+		elderGuardianOre = addOreConfig(builder, "Elder", "Guardian");
+
+		enderDragonOre = addOreConfig(builder, "Ender", "Dragon");
+
+		endermanOre = addOreConfig(builder, "Enderman");
+
 		endermiteOre = addOreConfig(builder, "Endermite");
+
+		evokerOre = addOreConfig(builder, "Evoker");
+
 		foxOre = addOreConfig(builder, "Fox");
+
 		ghastOre = addOreConfig(builder, "Ghast");
+
+		guardianOre = addOreConfig(builder, "Guardian");
+
+		horseOre = addOreConfig(builder, "Horse");
+
+		huskOre = addOreConfig(builder, "Husk");
+
+		ironGolemOre = addOreConfig(builder, "Iron", "Golem");
+
+		llamaOre = addOreConfig(builder, "Llama");
+
+		magmacubeOre = addOreConfig(builder, "Magma", "Cube");
+
+		mooshroomOre = addOreConfig(builder, "Mooshroom");
+
+		muleOre = addOreConfig(builder, "Mule");
+
+		ocelotOre = addOreConfig(builder, "Ocelot");
+
+		pandabearOre = addOreConfig(builder, "Panda", "Bear");
+
+		parrotOre = addOreConfig(builder, "Parrot");
+
+		phantomOre = addOreConfig(builder, "Phantom");
+
+		pigOre = addOreConfig(builder, "Pig");
+
 		pillagerOre = addOreConfig(builder, "Pillager");
+
+		polarbearOre = addOreConfig(builder, "Polar", "Bear");
+
+		pufferfishOre = addOreConfig(builder, "Pufferfish");
+
+		rabbitOre = addOreConfig(builder, "Rabbit");
+
 		ravagerOre = addOreConfig(builder, "Ravager");
+
+		salmonOre = addOreConfig(builder, "Salmon");
+
+		sheepOre = addOreConfig(builder, "Sheep");
+
 		shulkerOre = addOreConfig(builder, "Shulker");
+
+		silverfishOre = addOreConfig(builder, "Silverfish");
+
+		skeletonOre = addOreConfig(builder, "Skeleton");
+
+		skeletonHorseOre = addOreConfig(builder, "Skeleton", "Horse");
+
+		slimeOre = addOreConfig(builder, "Slime");
+
+		snowGolemOre = addOreConfig(builder, "Snow", "Golem");
+
+		spiderOre = addOreConfig(builder, "Spider");
+
+		squidOre = addOreConfig(builder, "Squid");
+
+		strayOre = addOreConfig(builder, "Stray");
+
+		turtleOre = addOreConfig(builder, "Turtle");
+
+		traderLlamaOre = addOreConfig(builder, "Trader", "Llama");
+
+		tropicalFishOre = addOreConfig(builder, "Tropical", "Fish");
+
+		vexOre = addOreConfig(builder, "Vex");
+
+		villagerOre = addOreConfig(builder, "Villager");
+
+		vindicatorOre = addOreConfig(builder, "Vindicator");
+
+		wanderingTraderOre = addOreConfig(builder, "Wandering", "Trader");
+
+		witchOre = addOreConfig(builder, "Witch");
+
+		witherOre = addOreConfig(builder, "Wither");
+
+		witherSkeletonOre = addOreConfig(builder, "Wither", "Skeleton");
+
+		wolfOre = addOreConfig(builder, "Wolf");
+
+		zombieOre = addOreConfig(builder, "Zombie");
+
+		zombieHorseOre = addOreConfig(builder, "Zombie", "Horse");
+
+		zombiePigmanOre = addOreConfig(builder, "Zombie", "Pigman");
+
+		zombieVillagerOre = addOreConfig(builder, "Zombie", "Villager");
 
 		builder.pop();
 
