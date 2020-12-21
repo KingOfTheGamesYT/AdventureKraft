1987c40b1ed993b82c9df0c3623634d76d909241
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2020-12-20 22:51:12 -0500

    added tool tip for uranium block
    and tool tip

--- src/main/java/com/devmaster/dangerzone/blocks/UraniumBlock.java
+++ src/main/java/com/devmaster/dangerzone/blocks/UraniumBlock.java
@@ -26,6 +26,6 @@ public class UraniumBlock extends Block {
 
     @Override
     public void addInformation(ItemStack stack, IBlockReader worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-       tooltip.add(new StringTextComponent(   "\u00A75" + "Amethyst blocks are seriously purple!" + "\u00A75"));
+       tooltip.add(new StringTextComponent(   "\u00A7e" + "Make a nice yellow block!" + "\u00A7e"));
     }
 }
