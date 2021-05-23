a3a2425266386246b0d885e536467c2a46a60a3b
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-05-22 20:21:08 -0400

    added Termite Token
    This is how you get to the crystal dimension

---
+++ src/main/java/com/devmaster/dangerzone/items/TermiteToken.java
@@ -0,0 +1,26 @@
+package com.devmaster.dangerzone.items;
+
+
+
+
+import com.devmaster.dangerzone.misc.DangerZone;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+
+public class TermiteToken extends Item {
+
+    public TermiteToken() {
+        super(new Properties().group(DangerZone.TAB));
+    }
+    @Override
+    public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn){
+        tooltip.add(new StringTextComponent("\u00A78" + "This is needed to get to the Crystal Dimension" + "\u00A78"));
+    }
+}
