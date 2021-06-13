5f3e7b3d1d43e3cbcf9017448ecb3f3ae2664443
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-06-13 01:12:56 -0400

    made butter no longer always edible
    same with cheese

--- src/main/java/net/Akio/CraftZone/Main/Foods/Butter.java
+++ src/main/java/net/Akio/CraftZone/Main/Foods/Butter.java
@@ -6,6 +6,6 @@ public class Butter extends ItemFood{
 
 	public Butter(int par1, int par2, float par3, boolean par4) {
 	       super(par2, par3, par4);
-	       setAlwaysEdible();
+
 	}
 }
--- src/main/java/net/Akio/CraftZone/Main/Foods/Cheese.java
+++ src/main/java/net/Akio/CraftZone/Main/Foods/Cheese.java
@@ -6,6 +6,6 @@ public class Cheese extends ItemFood{
 	
 	public Cheese(int par1, int par2, float par3, boolean par4) {
 	       super(par2, par3, par4);
-	       setAlwaysEdible();
+
 	}
 }
