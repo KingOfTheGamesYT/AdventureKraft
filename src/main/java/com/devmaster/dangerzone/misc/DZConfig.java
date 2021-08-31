65a6976874456b71e3a62d80876245dc141b00a4
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2021-08-31 16:30:10 -0400

    Made the config easier to read
    Now it's organized

--- src/main/java/com/devmaster/dangerzone/misc/DZConfig.java
+++ src/main/java/com/devmaster/dangerzone/misc/DZConfig.java
@@ -38,27 +38,36 @@ public class DZConfig {
         public final ForgeConfigSpec.ConfigValue<List<? extends String>> Entexclude;
 
         Spawn(ForgeConfigSpec.Builder builder) {
-            builder.push("Spawn Chances");
-            builder.comment("Configure youtubers spawn weight & min/max group size. Set weight to 0 to disable.");
+            builder.comment("Configure mob spawn weight (How frequently they spawn & min/max group size. Set weight to 0 to disable.");
+            builder.push("NotBreeBree Spawn Chances");
             NotBreeBreemin = builder.defineInRange("NotBreeBree Min", 1, 0, 640);
             NotBreeBreemax = builder.defineInRange("NotBreeBree Max", 2, 0, 640);
-            NotBreeBreeweight = builder.defineInRange("NotBreeBree Weight", 7, 0, 100);
+            NotBreeBreeweight = builder.defineInRange("NotBreeBree Weight", 100, 0, 100);
+             builder.pop();
+
+             builder.push("StampyLongNose Spawn Chances");
             StampyLongNosemin = builder.defineInRange("StampyLongNose Min", 1, 0, 64);
             StampyLongNosemax = builder.defineInRange("StampyLongNose Max", 2, 0, 64);
             StampyLongNoseweight = builder.defineInRange("StampyLongNose Weight", 7, 0, 100);
+            builder.pop();
+
+            builder.push("Tewity Spawn Chances");
             Tewitymin = builder.defineInRange("Tewity Min", 1, 0, 640);
             Tewitymax = builder.defineInRange("Tewity Max", 2, 0, 640);
             Tewityweight = builder.defineInRange("Tewity Weight", 7, 0, 100);
-            Entmin = builder.defineInRange("Ent Min", 1, 0, 64);
-            Entmax = builder.defineInRange("Ent Max", 1, 0, 64);
+             builder.pop();
+
+            builder.push("Ent Spawn Chances");
+            Entmin = builder.defineInRange("Ent Min", 1, 0, 640);
+            Entmax = builder.defineInRange("Ent Max", 1, 0, 640);
             Entweight = builder.defineInRange("Ent Weight", 7, 0, 100);
             builder.pop();
 
             builder.push("Spawnable Biomes");
             StampyLongNoseinclude = builder.defineList("include Stampy", Arrays.asList(SNOWY.toString(), MOUNTAIN.toString(), MESA.toString(), FOREST.toString(), HILLS.toString(), SANDY.toString(), SAVANNA.toString(), SWAMP.toString(), BEACH.toString(), PLAINS.toString()), o -> o instanceof String && (o.equals("") || BiomeDictionary.Type.getAll().contains(BiomeDictionaryHelper.getType(o.toString()))));
             StampyLongNoseexclude = builder.defineList("exclude Stampy", Arrays.asList(OCEAN.toString(), END.toString(), NETHER.toString()), o -> o instanceof String && (o.equals("") || BiomeDictionary.Type.getAll().contains(BiomeDictionaryHelper.getType(o.toString()))));
-            Tewityexclude = builder.defineList("include Tewity", Arrays.asList(SNOWY.toString(), MOUNTAIN.toString(), MESA.toString(), FOREST.toString(), HILLS.toString(), SANDY.toString(), SAVANNA.toString(), SWAMP.toString(), BEACH.toString(), PLAINS.toString()), o -> o instanceof String && (o.equals("") || BiomeDictionary.Type.getAll().contains(BiomeDictionaryHelper.getType(o.toString()))));
-            Tewityinclude = builder.defineList("exclude Tewity", Arrays.asList(OCEAN.toString(), END.toString(), NETHER.toString()), o -> o instanceof String && (o.equals("") || BiomeDictionary.Type.getAll().contains(BiomeDictionaryHelper.getType(o.toString()))));
+            Tewityinclude = builder.defineList("include Tewity", Arrays.asList(SNOWY.toString(), MOUNTAIN.toString(), MESA.toString(), FOREST.toString(), HILLS.toString(), SANDY.toString(), SAVANNA.toString(), SWAMP.toString(), BEACH.toString(), PLAINS.toString()), o -> o instanceof String && (o.equals("") || BiomeDictionary.Type.getAll().contains(BiomeDictionaryHelper.getType(o.toString()))));
+            Tewityexclude = builder.defineList("exclude Tewity", Arrays.asList(OCEAN.toString(), END.toString(), NETHER.toString()), o -> o instanceof String && (o.equals("") || BiomeDictionary.Type.getAll().contains(BiomeDictionaryHelper.getType(o.toString()))));
             NotBreeBreeinclude = builder.defineList("include NotBreeBree", Arrays.asList(SNOWY.toString(), MOUNTAIN.toString(), MESA.toString(), FOREST.toString(), HILLS.toString(), SANDY.toString(), SAVANNA.toString(), SWAMP.toString(), BEACH.toString(), PLAINS.toString()), o -> o instanceof String && (o.equals("") || BiomeDictionary.Type.getAll().contains(BiomeDictionaryHelper.getType(o.toString()))));
             NotBreeBreeexclude = builder.defineList("exclude NotBreeBree", Arrays.asList(OCEAN.toString(), END.toString(), NETHER.toString()), o -> o instanceof String && (o.equals("") || BiomeDictionary.Type.getAll().contains(BiomeDictionaryHelper.getType(o.toString()))));
             Entinclude = builder.defineList("include Ent", Arrays.asList(MOUNTAIN.toString(), FOREST.toString(), SAVANNA.toString(), SWAMP.toString()), o -> o instanceof String && (o.equals("") || BiomeDictionary.Type.getAll().contains(BiomeDictionaryHelper.getType(o.toString()))));
