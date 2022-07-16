f254a4c2980e776d173a8d6a3197b5c60eb0e2de
KingOfTheGamesYT <teamolympuscaptain@gmail.com>
2022-07-16 00:30:10 -0400

    improved Attack squid animations
    now doesn't rotate like crazy and is smooth

--- src/main/java/com/devmaster/dangerzone/client/render/model/AttackSquidModel.java
+++ src/main/java/com/devmaster/dangerzone/client/render/model/AttackSquidModel.java
@@ -98,16 +98,16 @@ public class AttackSquidModel<t> extends EntityModel<AttackSquid> {
 	@Override
 	public void setRotationAngles(AttackSquid entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
 
-		TentacleFront1.rotateAngleX = MathHelper.cos(limbSwingAmount * 0.25F * this.wingspeed) * 3.1415927F * 0.04F * limbSwing;
-		TentacleFront2.rotateAngleZ = MathHelper.cos(limbSwingAmount * 0.39F * this.wingspeed) * 3.1415927F * 0.04F * limbSwing;
-		TentacleBack1.rotateAngleX = MathHelper.cos(limbSwingAmount * 1.2F * this.wingspeed) * 3.1415927F * 0.4F * limbSwing;
-		TentacleBack2.rotateAngleX = MathHelper.cos(limbSwingAmount * 1.1F * this.wingspeed) * 3.1415927F * 0.4F * limbSwing;
-		TentacleLeft1.rotateAngleZ = MathHelper.cos(limbSwingAmount * 1.0F * this.wingspeed) * 3.1415927F * 0.4F * limbSwing;
-		TentacleLeft2.rotateAngleZ = MathHelper.cos(limbSwingAmount * 1.9F * this.wingspeed) * 3.1415927F * 0.4F * limbSwing;
-		TentacleLeft3.rotateAngleX = MathHelper.cos(limbSwingAmount * 1.8F * this.wingspeed) * 3.1415927F * 0.4F * limbSwing;
-		TentacleRight1.rotateAngleX = MathHelper.cos(limbSwingAmount * 1.7F * this.wingspeed) * 3.1415927F * 0.4F * limbSwing;
-		TentacleRight2.rotateAngleZ = MathHelper.cos(limbSwingAmount * 1.6F * this.wingspeed) * 3.1415927F * 0.4F * limbSwing;
-		TentacleRight3.rotateAngleX = MathHelper.cos(limbSwingAmount * 1.5F * this.wingspeed) * 3.1415927F * 0.4F * limbSwing;
+		TentacleFront1.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		TentacleFront2.rotateAngleZ = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		TentacleBack1.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		TentacleBack2.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		TentacleLeft1.rotateAngleZ = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		TentacleLeft2.rotateAngleZ = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		TentacleLeft3.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		TentacleRight1.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		TentacleRight2.rotateAngleZ = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+		TentacleRight3.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
 	}
 
 
