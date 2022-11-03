7bdd038c68ed6e3ba24b7f29f76f503e5147b3f1
Java sauce <mohammedbrandon204@gmail.com>
2022-11-03 17:44:37 -0400

    try to fix conflict[2]

--- src/main/java/com/devmaster/dangerzone/misc/DangerZone.java
+++ src/main/java/com/devmaster/dangerzone/misc/DangerZone.java
@@ -89,6 +89,27 @@ public class DangerZone {
             GlobalEntityTypeAttributes.put(RegistryHandler.RED_COW.get(), RedCow.getAttributes().create());
         });
     }
+
+
+    @SubscribeEvent
+	public static void addEntityAttributes(EntityAttributeCreationEvent event) {
+		event.put(RegistryHandler.TEWTIY.get(), Tewtiy.setCustomAttributes().create());
+		event.put(RegistryHandler.STAMPYLONGNOSE.get(), StampyLongNose.setCustomAttributes().create());
+		event.put(RegistryHandler.NOTBREEBREE.get(), NotBreeBree.setCustomAttributes().create());
+		event.put(RegistryHandler.REDROSEWARRIOR.get(), RedRoseWarrior.setCustomAttributes().create());
+		event.put(RegistryHandler.RAINBOWANT.get(), RainbowAnt.setCustomAttributes().create());
+		event.put(RegistryHandler.BUTTERFLY.get(), Butterfly.setCustomAttributes().create());
+		event.put(RegistryHandler.ENT.get(), Ent.setCustomAttributes().create());
+		event.put(RegistryHandler.CAVE_FISHER.get(), CaveFisher.setCustomAttributes().create());
+		event.put(RegistryHandler.ATTACK_SQUID.get(), AttackSquid.setCustomAttributes().create());
+		event.put(RegistryHandler.HYDROLISC.get(), Hydrolisc.setCustomAttributes().create());
+		event.put(RegistryHandler.ALLOSAURUS.get(), Allosaurus.setCustomAttributes().create());
+        event.put(RegistryHandler.BIRD.get(), Bird.setCustomAttributes().create());
+		event.put(RegistryHandler.TECHNOBLADE.get(), Technoblade.setCustomAttributes().create());
+		event.put(RegistryHandler.MERMAID.get(), Mermaid.setCustomAttributes().create());
+        
+	}
+    
     private void doClientStuff(final FMLClientSetupEvent event) {
         RenderTypeLookup.setRenderLayer(RegistryHandler.STICKY_BLOCK.get(), RenderType.getCutout());
         RenderTypeLookup.setRenderLayer(RegistryHandler.APPLE_LEAVES.get(), RenderType.getCutout());
