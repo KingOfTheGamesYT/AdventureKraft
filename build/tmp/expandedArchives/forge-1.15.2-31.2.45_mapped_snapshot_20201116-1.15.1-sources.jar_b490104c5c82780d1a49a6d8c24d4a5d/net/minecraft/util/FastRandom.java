d59d2d7cf8f6f77f534064e9272a36979b3bf6ff
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2023-03-20 18:34:09 -0400

    Updated forge
    no more security issues

--- build/tmp/expandedArchives/forge-1.15.2-31.2.45_mapped_snapshot_20201116-1.15.1-sources.jar_b490104c5c82780d1a49a6d8c24d4a5d/net/minecraft/util/FastRandom.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.util;
-
-public class FastRandom {
-   public static long mix(long left, long right) {
-      left = left * (left * 6364136223846793005L + 1442695040888963407L);
-      left = left + right;
-      return left;
-   }
-}
