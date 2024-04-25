c5ac5de28de457db836576edee809c9471b245d1
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2024-04-25 00:51:26 -0400

    added water dragon scale
    tofuu made a nice texture for it

---
+++ src/main/java/com/devmaster/dangerzone/items/WaterDragonScale.java
@@ -0,0 +1,23 @@
+package com.devmaster.dangerzone.items;
+
+
+import com.devmaster.dangerzone.misc.DangerZone;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class WaterDragonScale extends Item {
+
+    public WaterDragonScale() {
+        super(new Properties().group(DangerZone.TAB));
+    }
+    @Override
+    public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn){
+        tooltip.add(new StringTextComponent("\u00A7b" + "Dropped by the Water Dragon." + "\u00A7b"));
+    }
+}
