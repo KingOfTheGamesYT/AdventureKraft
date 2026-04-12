2d8981888f30f5d5ebacc6597e5d968a5c6bd990
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2026-04-12 18:51:36 -0400

    fixed a 6 year old bug
    Cave fishers spawn underground in the overworld like they should

--- src/main/java/com/devmaster/dangerzone/entity/CaveFisher.java
+++ src/main/java/com/devmaster/dangerzone/entity/CaveFisher.java
@@ -1,7 +1,5 @@
 package com.devmaster.dangerzone.entity;
 
-import com.devmaster.dangerzone.functions.CaveFisherOnInitialSpawnFunction;
-
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
@@ -14,15 +12,11 @@ import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.GolemEntity;
 import net.minecraft.entity.passive.fish.AbstractGroupFishEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.*;
-
-import net.minecraftforge.fml.network.FMLPlayMessages;
 
 import javax.annotation.Nullable;
 import java.util.*;
@@ -39,6 +33,7 @@ public class CaveFisher extends CreatureEntity {
     public void livingTick() {
         super.livingTick();
     }
+
     public static AttributeModifierMap.MutableAttribute getAttributes() {
         return MobEntity.func_233666_p_()
                 .createMutableAttribute(Attributes.MAX_HEALTH, 10)
@@ -46,6 +41,7 @@ public class CaveFisher extends CreatureEntity {
                 .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.35F)
                 .createMutableAttribute(Attributes.ATTACK_DAMAGE, 5);
     }
+
     @Override
     protected void registerGoals() {
         super.registerGoals();
@@ -57,26 +53,44 @@ public class CaveFisher extends CreatureEntity {
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, MonsterEntity.class, true));
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AnimalEntity.class, true));
-        this.targetSelector.addGoal(9, new NearestAttackableTargetGoal<>(this, StampyLongNose.class, true));
+        this.targetSelector.addGoal(9, new NearestAttackableTargetGoal<>(this, BaseYoutuber.class, true));
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, SlimeEntity.class, true));
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillagerEntity.class, true));
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, GolemEntity.class, true));
         this.targetSelector.addGoal(9, new NearestAttackableTargetGoal<>(this, AbstractGroupFishEntity.class, true));
-        this.targetSelector.addGoal(9, new NearestAttackableTargetGoal<>(this, NotBreeBree.class, true));
         this.targetSelector.addGoal(9, new NearestAttackableTargetGoal<>(this, RedRoseWarrior.class, true));
     }
+
+    public static boolean canSpawn(EntityType<CaveFisher> type, IWorld world, SpawnReason reason, BlockPos pos, Random random) {
+
+        if (pos.getY() > 60) return false;
+
+        if (world.canSeeSky(pos)) return false;
+
+        if (world.getLightFor(LightType.BLOCK, pos) > 7) return false;
+
+        if (!world.getBlockState(pos.down()).isOpaqueCube(world, pos.down())) return false;
+
+        if (!world.getBlockState(pos).isAir(world, pos)) return false;
+
+        return true;
+    }
+
     @Override
     protected SoundEvent getHurtSound(DamageSource damageSourceIn) {
         return SoundEvents.ENTITY_PLAYER_HURT;
     }
+
     @Override
     protected SoundEvent getDeathSound() {
         return SoundEvents.ENTITY_PLAYER_DEATH;
     }
+
     @Override
     protected float getSoundVolume() {
         return 0.8F;
     }
+
     @Override
     protected float getSoundPitch() {
         return 0.7F + rand.nextFloat() * 0.2F;
@@ -85,20 +99,5 @@ public class CaveFisher extends CreatureEntity {
     @Override
     protected boolean isDespawnPeaceful() {
         return true;
-    }
-    @Override
-    public ILivingEntityData onInitialSpawn(IServerWorld world, DifficultyInstance difficulty, SpawnReason reason,
-                                            @Nullable ILivingEntityData livingdata, @Nullable CompoundNBT tag) {
-        ILivingEntityData retval = super.onInitialSpawn(world, difficulty, reason, livingdata, tag);
-        double x = this.getPosX();
-        double y = this.getPosY();
-        double z = this.getPosZ();
-        Entity entity = this;
-
-        CaveFisherOnInitialSpawnFunction.executeProcedure(Stream
-                .of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("x", x), new AbstractMap.SimpleEntry<>("y", y),
-                        new AbstractMap.SimpleEntry<>("z", z), new AbstractMap.SimpleEntry<>("entity", entity))
-                .collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
-        return retval;
     }
 }
