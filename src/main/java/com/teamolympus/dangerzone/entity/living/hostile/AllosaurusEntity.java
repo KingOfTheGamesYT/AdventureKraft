112f9ca74e334696dfc5532dda6380171ace8e89
Java sauce <mohammedbrandon204@gmail.com>
2026-05-16 14:19:37 -0400

    OOPS

--- src/main/java/com/teamolympus/dangerzone/entity/living/hostile/AllosaurusEntity.java
+++ src/main/java/com/teamolympus/dangerzone/entity/living/hostile/AllosaurusEntity.java
@@ -21,6 +21,7 @@ public class AllosaurusEntity extends EntityMob implements IBossDisplayData {
         randomTextureSelector = rand.nextInt(AllosaurusRender.TEXTURES.length);
         this.tasks.addTask(0, new EntityAISwimming(this));
         this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
+        this.tasks.addTask(4, new EntityAIAttackOnCollide(this, EntityVillager.class, 1.0D, true));
         this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
         this.tasks.addTask(6, new EntityAIMoveThroughVillage(this, 1.0D, false));
         this.tasks.addTask(7, new EntityAIWander(this, 1.0D));
@@ -28,6 +29,7 @@ public class AllosaurusEntity extends EntityMob implements IBossDisplayData {
         this.tasks.addTask(8, new EntityAILookIdle(this));
         this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
         this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityVillager.class, 0, false));
     }
 
     public int getRandomTextureSelector() {
