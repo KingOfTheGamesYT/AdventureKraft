39c5a81e0d617d05f1a42625180c6b5a99b14ef4
Java sauce <mohammedbrandon204@gmail.com>
2026-05-17 19:28:48 -0400

    Update AllosaurusEntity.java

--- src/main/java/com/teamolympus/dangerzone/entity/living/hostile/AllosaurusEntity.java
+++ src/main/java/com/teamolympus/dangerzone/entity/living/hostile/AllosaurusEntity.java
@@ -21,17 +21,6 @@ public class AllosaurusEntity extends EntityMob implements IBossDisplayData {
         super(world);
         this.setSize(1.5f, 1.5f);
         this.experienceValue = 199;
-        this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
-        this.tasks.addTask(4, new EntityAIAttackOnCollide(this, EntityVillager.class, 1.0D, true));
-        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
-        this.tasks.addTask(6, new EntityAIMoveThroughVillage(this, 1.0D, false));
-        this.tasks.addTask(7, new DZAIWanderFrequent(this, 1.0D, 16));
-        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-        this.tasks.addTask(8, new EntityAILookIdle(this));
-        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
-        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
-        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityVillager.class, 0, false));
     }
 
     static final int TEXTURE = 22;
@@ -52,6 +41,10 @@ public class AllosaurusEntity extends EntityMob implements IBossDisplayData {
         this.dataWatcher.updateObject(TEXTURE, (byte)text);
     }
 
+    @Override
+    protected void updateEntityActionState() {
+        super.updateEntityActionState();
+    }
 
     @Override
     public IEntityLivingData onSpawnWithEgg(IEntityLivingData data) {
