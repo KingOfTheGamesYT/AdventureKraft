dff21a7c893fbe2516fd317f784be622ef0c9690
Java sauce <mohammedbrandon204@gmail.com>
2026-05-26 18:55:34 -0400

    changed to scorpion

--- src/main/java/com/teamolympus/dangerzone/client/model/ScorpionModel.java
+++ src/main/java/com/teamolympus/dangerzone/client/model/ScorpionModel.java
@@ -319,7 +319,7 @@ public class ScorpionModel extends ModelBase {
 
 	@Override
 	public void setRotationAngles(float p_78087_1_, float p_78087_2_, float p_78087_3_, float p_78087_4_, float p_78087_5_, float p_78087_6_, Entity p_78087_7_) {
-		float test = 0.5f;
+		float test = 0.3f;
 
 		float f9 = -(MathHelper.cos(p_78087_1_ * 0.6662F * 2.0F + 0.0F) * 0.4F) * p_78087_2_ * test;
 		float f10 = -(MathHelper.cos(p_78087_1_ * 0.6662F * 2.0F + DangerZone.PI) * 0.4F) * p_78087_2_ * test;
--- src/main/java/com/teamolympus/dangerzone/entity/living/hostile/ScorpionEntity.java
+++ src/main/java/com/teamolympus/dangerzone/entity/living/hostile/ScorpionEntity.java
@@ -1,5 +1,6 @@
 package com.teamolympus.dangerzone.entity.living.hostile;
 
+import com.teamolympus.dangerzone.entity.ai.DZAIWanderFrequent;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
@@ -23,7 +24,7 @@ public class ScorpionEntity extends EntityMob {
         this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
         this.tasks.addTask(3, new EntityAIMoveTowardsRestriction(this, 1.0D));
         this.tasks.addTask(4, new EntityAIMoveThroughVillage(this, 1.0D, false));
-        this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(5, new DZAIWanderFrequent(this, 1.0D, 14));
         this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         this.tasks.addTask(6, new EntityAILookIdle(this));
         this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
